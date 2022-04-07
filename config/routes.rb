4732d3eca31b338efc89177e9ed79bd67c8a0c55
tomoya.chihara <ctmajor07@gmail.com>
2022-04-07 05:56:05 +0000

    bootstrap_start

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  root to: 'toppages#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'topppages#index'
+  
 
   resources :movies
   resources :genres
