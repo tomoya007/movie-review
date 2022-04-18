c240e37d94ab9756710d71c7ec9023639e7f9aae
tomoya.chihara <ctmajor07@gmail.com>
2022-04-18 04:00:45 +0000

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +30,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation,)
   end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,4 +1,9 @@
 ActiveAdmin.setup do |config|
+  config.clear_stylesheets!
+  config.register_stylesheet 'active_admin'
+
+  config.clear_javascripts!
+  config.register_javascript 'active_admin'
   # == Site Title
   #
   # Set the title that is displayed on the main layout
