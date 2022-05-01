bcd04550e94e5b2d46b5b6a688216d990c5747b1
tomoya.chihara <ctmajor07@gmail.com>
2022-05-01 05:25:54 +0000

    login

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 
   gem 'pry-rails'
+
+  gem 'pry-nav'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'movies#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   
   resources :movies, only: [:index, :show]
