84f215b736e32c834c41bd58f17c20a8997cddd2
tomoya.chihara <ctmajor07@gmail.com>
2022-05-22 01:20:30 +0000

    raty.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 // 追記
 window.$ = window.jQuery = require('jquery'); //追記
-require('packs/raty') 
+require('raty') 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
