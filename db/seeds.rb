292d2db3e80f0492b6671606f9f7a7431b8719d4
tomoya.chihara <ctmajor07@gmail.com>
2023-04-17 15:13:36 +0900

    image English

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,7 @@ Movie.create!(
   [
     {
       title:'多日想果',
-  image:File.open('./app/assets/images/恋愛3.jpeg'),
+  image:File.open('./app/assets/images/love3.jpeg'),
   released_year:'2019',
   screening_time:'19',
   synposis:'台湾に憧れて留学したタカシ。日本では味わえなかった楽しい日々。そんなある日、屋台で働くフェンに恋をする。しかし、自分の気持ちを率直に伝えることができないまま時は流れ、恋は無情な形で幕を閉じることになる...',
@@ -54,7 +54,7 @@ Movie.create!(
     },
     {
       title:'大阪ハムレット',
-  image:File.open('./app/assets/images/poster23.jpeg'),
+  image:File.open('./app/assets/images/human4.jpeg'),
   released_year:'2008',
   screening_time:'107',
   synposis:'大阪の下町で暮らす久保家。３人の息子を抱え、一家の大黒柱となっているのは、働き者のお母ちゃん、房子。お父ちゃんが突然亡くなり、四十九日も済まないうちに、お父ちゃんの弟と名乗る叔父さんが転がり込んでくる…が、なぜかすんなりと受け入れる天真爛漫でのんきな房子。こうして５人の奇妙な家族生活が始まる。
@@ -64,7 +64,7 @@ Movie.create!(
     },
     {
       title:'チェルノブイリからの手紙',
-  image:File.open('./app/assets/images/ドキュメンタリー8.jpeg'),
+  image:File.open('./app/assets/images/documentary8.jpeg'),
   released_year:'2012',
   screening_time:'75',
   synposis:'1986年、過去最悪な原発事故が起きたチェルノブイリ。それから25年後に起きた福島原発事故。どちらの原発事故も最も深刻な暫定評価レベル7がくだされた。チェルノブイリに行けば27年後の福島の姿を見れるかもしれない。現在 福島で起きている事、今後福島が直面する問題、事故の対応、復興への道、命の重さ、次世代への願い、福島の人々を兄弟に思う彼らが 我々に”願い”を託してくれた。今、真実の言葉を聞く。',
@@ -82,7 +82,7 @@ Movie.create!(
     },
     {
       title:'ENOLA（エノーラ）',
-  image:File.open('./app/assets/images/アクション3.jpeg'),
+  image:File.open('./app/assets/images/action3.jpeg'),
   released_year:'2015',
   screening_time:'83',
   synposis:'名古屋市を中心に数万人が突然死する原因不明の事件が発生し、名古屋全域に避難命令が下った。非常事態に対応すべく特殊機関が設立され、銀色マスクを被った工作員が街を徘徊。市内に残った大学教授の平良と義理の妹のハヅキも工作員にマークされ、ハヅキは追い詰められることに。第六感がズバ抜けて強いハヅキは近くの不良少年健太へ自分の魂を移し逃げる。同時に、ハズキの魂の中で封印されていた“破壊の魂”が解放され、転移先の健太に凶悪な超能力が出現してしまう。暴走する健太（破壊の魂）を止める事ができるのか…。',
@@ -91,7 +91,7 @@ Movie.create!(
     },
     {
       title:'ユウカイ犯',
-  image:File.open('./app/assets/images/サスペンス7.jpeg'),
+  image:File.open('./app/assets/images/suspense7.jpeg'),
   released_year:'2018',
   screening_time:'42',
   synposis:'白昼堂々、若い娘をバンに押し込み連れ去った覆面の男達。それは人生を踏み外し、一発逆転を狙い犯行に及んだドン底の者達。計画は上手くいったかに思われたが、誘拐した女は標的にしていた娘の名前と違う。その娘の名前は凶悪で有名な大物ヤクザと同じ。絶望と焦燥から争い始める覆面の男達、そんな男達に女が放った言葉は「父を殺して下さい」というとんでもない依頼だった。',
@@ -100,7 +100,7 @@ Movie.create!(
     },
     {
       title:'ひとくず 新ディレクターズカット',
-  image:File.open('./app/assets/images/poster2.jpeg'),
+  image:File.open('./app/assets/images/drama9.jpeg'),
   released_year:'2019',
   screening_time:'123',
   synposis:'生まれてからずっと虐待されている少女・鞠のもとへ、犯罪を重ねる男・金田が空巣に入る。鞠の姿に幼い頃の自分を重ね、彼女を救おうと動き出す金田。鞠の母である凜の恋人から鞠が虐待を受けていることを知り、金田は凜の恋人を殺してしまう。凜もまた虐待されていた過去を持ち、子供の愛し方が分からない。そんな3人が不器用ながらも共に暮らし、「家族」の暖かさを感じ本物の「家族」へと近づいていく･･･。',
@@ -118,7 +118,7 @@ Movie.create!(
     },
     {
       title:'豆大福ものがたり',
-  image:File.open('./app/assets/images/コメディ7.jpeg'),
+  image:File.open('./app/assets/images/comedy7.jpeg'),
   released_year:'2013',
   screening_time:'20',
   synposis:'皆が3時のおやつを希望に生きる時代。「おやつ総選挙」を目前にマカロンやショートケーキなど、おやつ候補たちが選挙活動を繰り広げていた。
@@ -138,7 +138,7 @@ Movie.create!(
     },
     {
       title:'ビート・パー・MIZU',
-  image:File.open('./app/assets/images/恋愛5.jpeg'),
+  image:File.open('./app/assets/images/love5.jpeg'),
   released_year:'2019',
   screening_time:'27',
   synposis:'隅子はあらゆるBPMが瞬時にわかってしまうせいで不器用な人生を送っていた。しかしある日、友人に連れて行かれたライブハウスで男性・水と運命的な出会いを果たす。それ以来、隅子はBPMの研究と称して水の尾行を始める。',
@@ -147,7 +147,7 @@ Movie.create!(
     },
     {
       title:'私たちの家族',
-  image:File.open('./app/assets/images/ドキュメンタリー1.jpeg'),
+  image:File.open('./app/assets/images/documentary1.jpeg'),
   released_year:'2021',
   screening_time:'31',
   synposis:'エリンとみどりは結婚して20年、東京で3人の子供をもつ。2018年、エリンはアメリカで性別変更を申請し、日本でも性別移行と結婚書類の変更を進めた。しかし、日本では彼女の性転換が事実上の同性婚の容認につながるとして認められなく･･･
@@ -157,7 +157,7 @@ Movie.create!(
     },
     {
       title:'川凪ぐ火葬場',
-  image:File.open('./app/assets/images/アニメーション2.jpeg'),
+  image:File.open('./app/assets/images/animation2.jpeg'),
   released_year:'2022',
   screening_time:'13',
   synposis:'家族を亡くし、死を受け入れられない少年と、火葬場でたった一人死と向き合い続けている老人が出会う。
@@ -168,7 +168,7 @@ Movie.create!(
     },
     {
       title:'キドラアローン　破壊の娘',
-  image:File.open('./app/assets/images/アクション6.jpeg'),
+  image:File.open('./app/assets/images/animation6.jpeg'),
   released_year:'2019',
   screening_time:'80',
   synposis:'荒廃した日本。とある山奥の田舎町、セーラー服姿で学校に向かうキドラを町民たちは笑顔で見送っていた。しかし、夕方になると町民たちは一変して鬼の形相でキドラに襲いかかる。そんな彼らをキドラは12歳とは思えない身のこなしで倒してしまう。そして偶然出会ったヒットマンを連れまた学校へと向かう……。やがて、キドラの正体と日中は平和を偽る町の全貌が明らかに!',
@@ -196,7 +196,7 @@ Movie.create!(
     },
     {
       title:'one two three',
-  image:File.open('./app/assets/images/コメディ1.jpeg'),
+  image:File.open('./app/assets/images/comedy1.jpeg'),
   released_year:'2018',
   screening_time:'21',
   synposis:'大学寮に住む篠原は、バンドで作詞とドラムを担当し、ライブを3日後に控えていた。この日、思いを寄せる井上から夕食に誘われた篠原は井上に宛てた手紙をしのばせる。そして迎えた夕食の席で井上からある人に向けて代筆の依頼をされる。篠原は好きな人からのお願いをどうするのか……',
@@ -214,7 +214,7 @@ Movie.create!(
     },
     {
       title:'惑星ミズサ',
-  image:File.open('./app/assets/images/恋愛9.jpeg'),
+  image:File.open('./app/assets/images/love9.jpeg'),
   released_year:'2014',
   screening_time:'96',
   synposis:'田舎町にあるちょうちん屋の一家に生まれ、美しい風景に囲まれながらも退屈な日々を過ごしているヨシスケ。ふらっと入った風俗店で、自分は宇宙人だと言い張る風俗嬢のミズサと出会う。ミズサに特別な感情を抱くようになったヨシスケは、風俗嬢を辞めてほしいと頼む。風俗を辞めると地球が滅亡すると語るミズサだったが、それを聞き入れることに。しかし、彼女の言葉を裏付けるように謎の黒い物体が地球を覆っていき…。',
@@ -223,7 +223,7 @@ Movie.create!(
     },
     {
       title:'ストロボライト',
-  image:File.open('./app/assets/images/サスペンス4.jpeg'),
+  image:File.open('./app/assets/images/suspense4.jpeg'),
   released_year:'2013',
   screening_time:'122',
   synposis:'閑静な住宅街で起こった殺人事件。それは手首を切り落とすという猟奇的なものだった。
@@ -234,7 +234,7 @@ Movie.create!(
     },
     {
       title:'大鹿村から吹くパラム',
-  image:File.open('./app/assets/images/ドキュメンタリー7.jpeg'),
+  image:File.open('./app/assets/images/documentary7.jpeg'),
   released_year:'2021',
   screening_time:'87',
   synposis:'南アルプスが美しく望めるここは長野県下伊那郡大鹿村の釜沢。イギリスから移住してきたサイモン･ピゴットさんを中心に12人の住民が自然とのスローライフな生活を楽しんでいる。谷口昇さんは釜沢のシンボルである桜の木が切られていたのを発見する。実は、5年前の南アルプスを貫くトンネルの起工式以降、大鹿村ではリニア中央新幹線の工事が始まっていたのだ。第32回東京学生映画祭 コンペティション部門グランプリ受賞作品',
@@ -243,7 +243,7 @@ Movie.create!(
     },
     {
       title:'カパエマフの魔法石',
-  image:File.open('./app/assets/images/アニメーション3.jpeg'),
+  image:File.open('./app/assets/images/animation3.jpeg'),
   released_year:'2020',
   screening_time:'8',
   synposis:'調査に10年の年月を費やし、美しいアニメーションと、力強い音楽とともに届けられる本作。「アロハの心をうたい継ぐ者 / Kumu Hina」のヒナ先生こと、ヒナレイモアナ・ウォン・カル。性的少数者として、また民族的少数者としてハワイの社会で声を上げ続けてきたヒナが「私たちの歴史を、私たちの言葉で、そして私たちで語り直す」と、次世代に贈る物語。
