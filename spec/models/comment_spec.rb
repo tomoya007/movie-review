0e627edb9e7127eb63880aba345090b62bd6cf61
tomoya.chihara <ctmajor07@gmail.com>
2023-05-02 20:33:58 +0900

    comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   describe "バリデーション" do
     context "異常系" do
-      # it 'image必須' do
-      #   comment = build(:comment, images: nil)
-      #   comment.valid?
-      #   expect(comment.errors[:images]).to include('を入力してください')
-      # end
+      it 'raty必須' do
+        comment = build(:comment, raty: nil)
+        comment.valid?
+        expect(comment.errors[:raty]).to include('を入力してください')
+      end
 
       it 'content必須' do
         comment = build(:comment, content: nil)
@@ -21,11 +21,11 @@ RSpec.describe Comment, type: :model do
         expect(comment.errors[:content]).to include('は250文字以内で入力してください')
       end
 
-      # it 'contentが250文字以下の場合' do
-      #   comment = build(:comment, content: 'a'*12)
-      #   comment.valid?
-      #   expect(comment.save).to eq true
-      # end
+      it 'contentが250文字以下の場合' do
+        comment = build(:comment, content: 'a'*12)
+        comment.valid?
+        expect(comment.save).to eq true
+      end
     end
   end 
 end
