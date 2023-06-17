7d3bf4c6caf414785f1fa255ff624c65168c8f82
tomoya.chihara <ctmajor07@gmail.com>
2023-06-18 05:48:06 +0900

    comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user,   only: :destroy
+  before_action :set_comment, only: :destroy
+  before_action :correct_user, only: :destroy
 
   def create
-    @comment = current_user.comments.new(comment_params) 
-    @movie = Movie.find_by(params[:id])
+    @comment = current_user.comments.build(comment_params) 
     if @comment.save
       flash[:success] = 'コメントを投稿しました。'
     else
@@ -20,12 +20,18 @@ class CommentsController < ApplicationController
   end
 
   private
-    def correct_user
-      @comment = current_user.comments.find_by(id: params[:id])
-      redirect_back fallback_location: root_path if @comment.nil?
+  
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+  end
+
+  def correct_user
+    unless @comment&.user == current_user
+      redirect_back fallback_location: root_path 
     end
-    
-    def comment_params
-      params.require(:comment).permit(:movie_id, :user_id, :content, :raty) 
-    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:movie_id, :content, :raty) 
+  end
 end
